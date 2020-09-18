42802244acd98060866c394b18c23911d5c0e324
erkin <ax.erkin@gmail.com>
2020-09-18 12:34:47 +0500

    ClubOlympusApp

--- app/src/main/java/com/example/clubolimpus/AddMemberActivity.java
+++ app/src/main/java/com/example/clubolimpus/AddMemberActivity.java
@@ -1,9 +1,11 @@
 package com.example.clubolimpus;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NavUtils;
-
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.Menu;
@@ -13,14 +15,28 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-import java.util.ArrayList;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.CursorLoader;
+import androidx.loader.content.Loader;
 
-public class AddMemberActivity extends AppCompatActivity {
+import com.example.clubolimpus.data.ClubOlympusContract.MemberEntry;
+
+public class AddMemberActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<Cursor>{
+
+    private static final int EDIT_MEMBER_LOADER = 111;
+    Uri currentMemberUri;
 
     private EditText firstNameEditText;
     private EditText lastNameEditText;
-    private EditText groupEditText;
+    private EditText sportEditText;
     private Spinner genderSpinner;
     private int gender = 0;
     private ArrayAdapter spinnerAdapter;
@@ -31,9 +47,22 @@ public class AddMemberActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_member);
 
+        Intent intent = getIntent();
+
+        currentMemberUri = intent.getData();
+
+        if (currentMemberUri == null) {
+            setTitle("Add a Memeber");
+            invalidateOptionsMenu();
+        } else {
+            setTitle("Edit the Memeber");
+            getSupportLoaderManager().initLoader(EDIT_MEMBER_LOADER,
+                    null, this);
+        }
+
         firstNameEditText = findViewById(R.id.firstNameEditText);
         lastNameEditText = findViewById(R.id.lastNameEditText);
-        groupEditText = findViewById(R.id.groupEditText);
+        sportEditText = findViewById(R.id.sportEditText);
         genderSpinner = findViewById(R.id.genderSpinner);
 
 
@@ -49,11 +78,11 @@ public class AddMemberActivity extends AppCompatActivity {
                 String selectedGender = (String) parent.getItemAtPosition(position);
                 if (!TextUtils.isEmpty(selectedGender)) {
                     if (selectedGender.equals("Male")) {
-                        gender = 1;
+                        gender = MemberEntry.GENDER_MALE;
                     } else if (selectedGender.equals("Female")) {
-                        gender = 2;
+                        gender = MemberEntry.GENDER_FEMALE;
                     } else {
-                        gender = 0;
+                        gender = MemberEntry.GENDER_UNKNOWN;
                     }
                 }
             }
@@ -63,6 +92,20 @@ public class AddMemberActivity extends AppCompatActivity {
                 gender = 0;
             }
         });
+
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+
+        super.onPrepareOptionsMenu(menu);
+
+        if (currentMemberUri == null) {
+            MenuItem menuItem = menu.findItem(R.id.delete_member);
+            menuItem.setVisible(false);
+        }
+
+        return true;
     }
 
     @Override
@@ -75,13 +118,189 @@ public class AddMemberActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.save_member:
+                saveMember();
                 return  true;
             case R.id.delete_member:
+                showDeleteMemberDialog();
                 return true;
             case android.R.id.home:
                 NavUtils.navigateUpFromSameTask(this);
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void saveMember() {
+
+        String firstName = firstNameEditText.getText().toString().trim();
+        String lastName = lastNameEditText.getText().toString().trim();
+        String sport = sportEditText.getText().toString().trim();
+
+        if (TextUtils.isEmpty(firstName)) {
+            Toast.makeText(this,
+                    "Input the first name",
+                    Toast.LENGTH_LONG).show();
+            return;
+        } else if (TextUtils.isEmpty(lastName)) {
+            Toast.makeText(this,
+                    "Input the last name",
+                    Toast.LENGTH_LONG).show();
+            return;
+        } else if (TextUtils.isEmpty(sport)) {
+            Toast.makeText(this,
+                    "Input the sport",
+                    Toast.LENGTH_LONG).show();
+            return;
+        } else if (gender == MemberEntry.GENDER_UNKNOWN) {
+            Toast.makeText(this,
+                    "Choose the gender",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MemberEntry.COLUMN_FIRST_NAME, firstName);
+        contentValues.put(MemberEntry.COLUMN_LAST_NAME, lastName);
+        contentValues.put(MemberEntry.COLUMN_SPORT, sport);
+        contentValues.put(MemberEntry.COLUMN_GENDER, gender);
+
+        if (currentMemberUri == null) {
+            ContentResolver contentResolver = getContentResolver();
+            Uri uri = contentResolver.insert(MemberEntry.CONTENT_URI,
+                    contentValues);
+
+            if (uri == null) {
+                Toast.makeText(this,
+                        "Insertion of data in the table failed",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this,
+                        "Data saved", Toast.LENGTH_LONG).show();
+
+            }
+        } else {
+            int rowsChanged = getContentResolver().update(currentMemberUri,
+                    contentValues, null, null);
+
+            if (rowsChanged == 0) {
+                Toast.makeText(this,
+                        "Saving of data in the table failed",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this,
+                        "Member updated", Toast.LENGTH_LONG).show();
+            }
+        }
+
+
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+
+        String[] projection = {
+                MemberEntry._ID,
+                MemberEntry.COLUMN_FIRST_NAME,
+                MemberEntry.COLUMN_LAST_NAME,
+                MemberEntry.COLUMN_GENDER,
+                MemberEntry.COLUMN_SPORT
+        };
+
+        return new CursorLoader(this,
+                currentMemberUri,
+                projection,
+                null,
+                null,
+                null
+        );
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
+        if (cursor.moveToFirst()) {
+            int firstNameColumIndex = cursor.getColumnIndex(
+                    MemberEntry.COLUMN_FIRST_NAME
+            );
+            int lastNameColumIndex = cursor.getColumnIndex(
+                    MemberEntry.COLUMN_LAST_NAME
+            );
+            int genderColumIndex = cursor.getColumnIndex(
+                    MemberEntry.COLUMN_GENDER
+            );
+            int sportColumIndex = cursor.getColumnIndex(
+                    MemberEntry.COLUMN_SPORT
+            );
+
+            String firstName = cursor.getString(firstNameColumIndex);
+            String lastName = cursor.getString(lastNameColumIndex);
+            int gender = cursor.getInt(genderColumIndex);
+            String sport = cursor.getString(sportColumIndex);
+
+            firstNameEditText.setText(firstName);
+            lastNameEditText.setText(lastName);
+            sportEditText.setText(sport);
+
+            switch (gender) {
+                case MemberEntry.GENDER_MALE:
+                    genderSpinner.setSelection(1);
+                    break;
+                case MemberEntry.GENDER_FEMALE:
+                    genderSpinner.setSelection(2);
+                    break;
+                case MemberEntry.GENDER_UNKNOWN:
+                    genderSpinner.setSelection(0);
+                    break;
+
+            }
+        }
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
+    }
+
+    private void showDeleteMemberDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Do you want delete the member?");
+        builder.setPositiveButton("Delete",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        deleteMember();
+                    }
+                });
+        builder.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (dialog != null) {
+                            dialog.dismiss();
+                        }
+                    }
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+    }
+
+    private void deleteMember() {
+        if (currentMemberUri != null) {
+            int rowsDeleted = getContentResolver().delete(currentMemberUri,
+                    null, null);
+
+            if (rowsDeleted == 0) {
+                Toast.makeText(this,
+                        "Deleting of data from the table failed",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this,
+                        "Member is deleted",
+                        Toast.LENGTH_LONG).show();
+            }
+
+            finish();
+        }
     }
 }
